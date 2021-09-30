0d23520673b8c1ef2feab2b19d9aa1a11845eb95
shajal-kumer <shajalkumer@gmail.com>
2021-09-30 19:07:27 +0600

    lession 8 - Routin setup

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classes from "../styles/Account.module.css";
 export default function Account() {
 	return (
@@ -6,7 +7,9 @@ export default function Account() {
 			<span className="material-icons-outlined" title="Account">
 				account_circle
 			</span>
-			<a href="signup.html">Signup</a>
+
+			<Link to="/signup">Signup</Link>
+			<Link to="/login">Login</Link>
 			{/* <span className="material-icons-outlined" title="Logout"> logout </span> --> */}
 		</div>
 	);
