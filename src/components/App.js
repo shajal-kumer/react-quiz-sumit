f3f6f7466ae3665a6214eec08e0146d2e25b1cdf
shajal-kumer <shajalkumer@gmail.com>
2021-09-30 17:06:07 +0600

    login component done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import "../styles/App.css";
 import Layout from "./Layout";
-import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+// import Signup from "./pages/Signup";
 // import Home from "./pages/Home";
 
 function App() {
 	return (
 		<Layout>
-			<Signup />
+			{/* <Signup /> */}
 			{/* <Home /> */}
+			<Login />
 		</Layout>
 	);
 }
---
+++ src/components/pages/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import classes from "../../styles/Login.module.css";
+import Button from "../Button";
+import Form from "../Form";
+import Illustration from "../Illustration";
+import TextInput from "../TextInput";
+
+export default function Login() {
+	return (
+		<>
+			<h1>Login to your account</h1>
+			<div className="column">
+				<Illustration />
+				<Form className={classes.login}>
+					<TextInput type="email" placeholder="Enter Email" icon="alternate_email" />
+					<TextInput type="password" placeholder="Enter Password" icon="lock" />
+
+					<Button>Submit Now</Button>
+
+					<div className="info">
+						Already have an account? <a href="login.html">Login</a> instead.
+					</div>
+				</Form>
+			</div>
+		</>
+	);
+}
