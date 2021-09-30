7786183f34ef1ea4c192aaf1dd4cf8cbc9faaac1
shajal-kumer <shajalkumer@gmail.com>
2021-09-30 14:06:22 +0600

    lession-4 done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,12 @@
+import "../styles/App.css";
+import Layout from "./Layout";
+import Home from "./pages/Home";
+
 function App() {
 	return (
-		<div className="App">
-			<h1>Hello world</h1>
-		</div>
+		<Layout>
+			<Home />
+		</Layout>
 	);
 }
 
